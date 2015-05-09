50ca7d66c7c83cd38f3794b24ed7bc3519904dce
Meetic666 <bellay.quentin@gmail.com>
2015-05-09 17:23:50 -0400

    Added few platform for tables and Couch
    Added camera follow behaviour

---
+++ Project/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public GameObject m_Target;
+
+	public float m_CameraSpeed;
+	public float m_CameraAcceleration;
+
+	public Vector2 m_CameraThreshold;
+
+	Vector3 m_CurrentSpeed;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += m_CurrentSpeed * Time.deltaTime;
+
+		Vector3 targetOffset = m_Target.transform.position - transform.position;
+
+		Vector3 targetSpeed = Vector3.zero;
+
+		if(Mathf.Abs (targetOffset.x) > m_CameraThreshold.x)
+		{
+			targetSpeed.x = Mathf.Sign(targetOffset.x) * m_CameraSpeed;
+		}
+
+		if(Mathf.Abs (targetOffset.y) > m_CameraThreshold.y)
+		{
+			targetSpeed.y = Mathf.Sign(targetOffset.y) * m_CameraSpeed;
+		}
+
+		m_CurrentSpeed = Vector3.Lerp(m_CurrentSpeed, targetSpeed, m_CameraAcceleration * Time.deltaTime);
+	}
+}
