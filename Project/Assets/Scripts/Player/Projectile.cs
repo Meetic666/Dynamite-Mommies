b32df409e2210867556773afe7bbd5aa6f31f032
Meetic666 <bellay.quentin@gmail.com>
2015-05-09 15:36:15 -0400

    Modified projectile to use different tags to ignore collision with

--- Project/Assets/Scripts/Player/Projectile.cs
+++ Project/Assets/Scripts/Player/Projectile.cs
@@ -8,14 +8,40 @@ public class Projectile : MonoBehaviour
 
 	int m_Direction;
 	
+	public GameObject m_SplatterParticlesPrefab;
+
+	public string m_TagToIgnore;
+	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position += Vector3.right * m_Direction * m_Speed;
+		Vector3 previousPosition = transform.position;
+		transform.position += Vector3.right * m_Direction * m_Speed * Time.deltaTime;
+
+		RaycastHit hitInfo;
+
+		if(Physics.Raycast(previousPosition, Vector3.right * m_Direction, out hitInfo, m_Speed * Time.deltaTime))
+		{
+			transform.position = hitInfo.point;
+
+			OnTriggerEnter(hitInfo.collider);
+		}
 	}
 
 	public void SetDirection(int direction)
 	{
 		m_Direction = direction;
+	}
+	
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		if(otherCollider.tag != m_TagToIgnore)
+		{
+			// Check for health component
+			
+			Destroy(gameObject);
+			
+			Instantiate(m_SplatterParticlesPrefab, transform.position, Quaternion.identity);
+		}
 	}
 }
