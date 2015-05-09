e345ffa1a99e078a4d6cacdb081be318fa01817f
pryd0008 <pryd0008@algonquinlive.com>
2015-05-09 15:08:06 -0400

    - Made the DealDamage() in Base_AI virtual instead of abstract - Created Drunk_Friend script and implemented movement and ledge detection - Create Ai testing scene

--- Project/Assets/Scripts/AI/Base_AI.cs
+++ Project/Assets/Scripts/AI/Base_AI.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public abstract class Base_AI 
+public abstract class Base_AI : MonoBehaviour 
 {
 	protected int m_Health;
+	protected float m_MovementSpeed;
+
 	protected enum States
 	{
 		e_Idle = 0,
 		e_Patrol,
 		e_Attack,
+		e_Dead,
 		e_SpecialOne
 	}
 	protected States m_CurrentState = States.e_Idle;
 
 	protected abstract void ChangeStateTo (States newState);
 	protected abstract void TakeDamage (int dmg);
-	protected abstract void DealDamage (int dmg);
+
+	protected virtual void DealDamage (int dmg);
 
 	protected virtual void TriggerPatrol ()
 	{
@@ -35,5 +39,10 @@ public abstract class Base_AI
 	protected virtual void TriggerSpecialOne ()
 	{
 		m_CurrentState = States.e_SpecialOne;
+	}
+
+	protected virtual void KillSelf()
+	{
+		m_CurrentState = States.e_Dead;
 	}
 }
---
+++ Project/Assets/Scripts/AI/Drunk_Friend.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drunk_Friend : Base_AI 
+{
+	private float m_DelayUntilStumble;
+	private RaycastHit m_RayHit;
+
+	public float m_InitialMovementSpeed;
+	public float m_MaxStepHeight;
+
+	void Start()
+	{
+		m_MovementSpeed = m_InitialMovementSpeed;
+	}
+
+	void Update()
+	{
+		if(Physics2D.Raycast(transform.position + (transform.right * 0.6f), -Vector2.up, m_MaxStepHeight))
+		{
+			transform.position += (transform.right * m_MovementSpeed);
+		}
+		else
+		{
+			TurnAround();
+		}
+	}
+
+	void TurnAround()
+	{
+		transform.Rotate (Vector3.up * 180);
+	}
+
+	protected override void ChangeStateTo(Base_AI.States newState)
+	{
+		switch(newState)
+		{
+			case States.e_Idle:
+			{
+				break;
+			}
+			case States.e_Patrol:
+			{
+				break;
+			}
+			case States.e_Attack:
+			{
+				break;
+			}
+			case States.e_Dead:
+			{
+				break;
+			}
+			case States.e_SpecialOne:
+			{
+				break;
+			}
+		default:
+			break;
+		}
+
+		m_CurrentState = newState;
+	}
+
+	protected override void TakeDamage(int dmg)
+	{
+		m_Health -= dmg;
+
+		if(m_Health <= 0)
+		{
+			KillSelf();
+		}
+	}
+
+	protected override void KillSelf()
+	{
+		base.KillSelf ();
+	}
+}
